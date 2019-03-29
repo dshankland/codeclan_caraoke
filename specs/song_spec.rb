680167cf695abd07f4cbd5a7db04e6a36fa1ecda
Darren Shankland <ds@4pq.co.uk>
2019-03-29 11:41:51 +0000

    Initial commit - added song class

---
+++ specs/song_spec.rb
@@ -0,0 +1,15 @@
+require('minitest/autorun')
+require('minitest/rg')
+require_relative('../song')
+
+class SongTest < MiniTest::Test
+
+  def setup()
+    @song1 = Song.new("Ooh La La")
+  end
+
+  def test_song()
+    assert_equal("Ooh La La", @song1.title)
+  end
+
+end
